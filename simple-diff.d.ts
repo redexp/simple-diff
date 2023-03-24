51bfed7f8d17e22b6e8eeadfe005df79f0f568db
Sergii Kliuchnyk <redexp@ukr.net>
2023-03-24 12:20:18 +0200

    added types

---
+++ simple-diff.d.ts
@@ -0,0 +1,31 @@
+declare function diff(oldObj: any, newObj: any, ops?: DiffOptions): DiffEvent[];
+
+export default diff;
+
+export interface DiffOptions extends Partial<PathChange> {
+	idProp?: string,
+	addEvent?: string,
+	removeEvent?: string,
+	changeEvent?: string,
+	addItemEvent?: string,
+	removeItemEvent?: string,
+	moveItemEvent?: string,
+	callback?: (event: DiffEvent) => void,
+	comparators?: Array<[any, Comparator]>,
+	ignore?: Comparator,
+}
+
+export type Path = Array<string | number>;
+
+export interface PathChange {
+	oldPath: Path,
+	newPath: Path,
+}
+
+export type Comparator = (oldValue: any, newValue: any, options: PathChange) => boolean;
+
+export interface DiffEvent extends PathChange {
+	type: 'add' | 'remove' | 'change' | 'add-item' | 'remove-item' | 'move-item',
+	oldValue: any,
+	newValue: any,
+}
